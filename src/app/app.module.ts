aa17ff3a7b1994c4416563dca927211deae470b9
Njoro410 <brian.njoroge@student.moringaschool.com>
2022-02-12 08:32:20 +0300

    added component for each section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { UserDetailsComponent } from './components/user-details/user-details.component';
+import { UserProfileComponent } from './components/user-profile/user-profile.component';
+import { RepositoriesComponent } from './components/repositories/repositories.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    UserDetailsComponent,
+    UserProfileComponent,
+    RepositoriesComponent
   ],
   imports: [
     BrowserModule,
