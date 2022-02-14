1765ba6640827d5404af8b16f296a52550dd4127
Njoro410 <brian.njoroge@student.moringaschool.com>
2022-02-14 08:09:52 +0300

    added cards to show repos

--- src/app/repo.ts
+++ src/app/repo.ts
@@ -2,10 +2,10 @@ export class Repo {
     constructor(
         public name:string,
         public html_url:string,
-        public description:string
-        // public created_at:Date,
-        // public forks:number,
-        // public watchers_count:number, 
-        // public language:string
+        public description:string,
+        public forks:number,
+        public watchers_count:number, 
+        public language:string,
+        public updated_at:Date,
         ){}
   }
